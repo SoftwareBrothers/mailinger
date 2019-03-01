b6008a1b4bcd165b7ad58cc68f65184c5caeb6af
Paweł Lorenc <pawel.lorenc@rst-it.com>
2019-03-01 14:06:42 +0100

    sned email WIP

--- src/components/SendEmailButton.tsx
+++ src/components/SendEmailButton.tsx
@@ -2,14 +2,45 @@ import Button from '@material-ui/core/Button';
 import React from 'react';
 import {UserCtx} from "../contexts/user.context";
 import send from '../services/MailSender';
+import {IRecipient} from "../types/recipient";
 
 const SendEmailButton = () => {
 
-  const user = React.useContext(UserCtx);
+  const [user] = React.useContext(UserCtx);
 
   const click = () => {
-    send([], [], user);
-  }
+    const recipients: IRecipient[] = [
+      {
+        data: {
+          content: 'Masz te swoje 20k.',
+          title: 'Wystaw fakturę',
+        },
+        email: 'pawel.lorenc@rst-it.com',
+        firstName: 'Paweł',
+        lastName: 'Lorenc',
+      },
+      {
+        data: {
+          content: 'Masz te swoje 30k.',
+          title: 'Wystaw fakturę',
+        },
+        email: 'marek.firlejczyk@rst-it.com',
+        firstName: 'Marek',
+        lastName: 'Firlejczyk',
+      },
+      {
+        data: {
+          content: 'Masz te swoje 40k.',
+          title: 'Wystaw fakturę',
+        },
+        email: 'szymon.poltorak@rst-it.com',
+        firstName: 'Szymon',
+        lastName: 'Półtorak',
+      },
+    ];
+
+    send(recipients, user);
+  };
 
   return (
     <Button variant="contained" color="primary" onClick={click}>
--- src/services/MailSender.ts
+++ src/services/MailSender.ts
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import {IUser} from "../types";
-import {IEmailMeta} from "../types/email";
 import {IRecipient} from "../types/recipient";
 
-const send = (data: IEmailMeta[], recipients: IRecipient[], user: IUser) => {
+const send = (recipients: IRecipient[], user: IUser) => {
+  console.log(recipients);
   console.log(user);
+
+  const data = 'RnJvbTogcGF3ZWwubG9yZW5jQHJzdC1pdC5jb20KVG86ICBtYXJlay5maXJsZWpjenlrQHJzdC1pdC5jb20KU3ViamVjdDogU3ViamVjdCBUZXh0CgpUaGUgbWVzc2FnZSB0ZXh0IGdvZXMgaGVyZQ==';
+
+  axios.post('https://www.googleapis.com/gmail/v1/users/me/messages/send',
+    { raw: data},
+    { headers: {'Authorization': 'Bearer '+user.token.accessToken} }
+    )
+    .then(res => {
+        console.log(res);
+    }).catch((error) => {
+      console.log(error);
+  });
 };
 
 export default send;

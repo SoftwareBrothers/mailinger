e54e0cb170caf70c83a825fd5582df41772e2e7c
Marek <marek.firlejczyk@rst-it.com>
2019-03-01 19:18:25 +0100

    Odrder picker

--- src/components/Stepper/Steps.tsx
+++ src/components/Stepper/Steps.tsx
@@ -2,6 +2,8 @@ import Step from '@material-ui/core/Step';
 import StepButton from '@material-ui/core/StepButton';
 import Stepper from '@material-ui/core/Stepper';
 import React from 'react';
+import DrivePicker from 'src/components/DrivePicker';
+import { SpreadsheetCtx } from 'src/contexts/spreadsheet.context';
 import { StepCtx } from 'src/contexts/step.context';
 import Editor from '../Editor/Editor';
 import Sender from './../Sender';
@@ -9,6 +11,8 @@ import Navigation from './Navigation';
 
 const Steps = () => {
   const [activeStep, setActiveStep] = React.useState(1);
+  const [spreadsheet, setSpreadsheet] = React.useState(null);
+
   const steps = [
     { key: 'choose', label: 'Choose' },
     { key: 'prepare_template', label: 'Prepare template' },
@@ -17,25 +21,35 @@ const Steps = () => {
 
   function getComponent() {
     switch (activeStep) {
+      case 1:
+        return <DrivePicker />;
       case 2:
         return <Editor />;
       case 3:
         return <Sender />;
+      default:
+        return null;
     }
   }
 
   return (
-    <StepCtx.Provider value={[activeStep, setActiveStep]}>
-      <Stepper alternativeLabel={true} nonLinear={true} activeStep={activeStep}>
-        {steps.map(step => {
-          <Step key={step.key}>
-            <StepButton>{step.label}</StepButton>
-          </Step>;
-        })}
-      </Stepper>
-      {getComponent()}
-      <Navigation />
-    </StepCtx.Provider>
+    <SpreadsheetCtx.Provider value={[spreadsheet, setSpreadsheet]}>
+      <StepCtx.Provider value={[activeStep, setActiveStep]}>
+        <Stepper
+          alternativeLabel={true}
+          nonLinear={true}
+          activeStep={activeStep}
+        >
+          {steps.map(step => {
+            <Step key={step.key}>
+              <StepButton>{step.label}</StepButton>
+            </Step>;
+          })}
+        </Stepper>
+        {getComponent()}
+        <Navigation />
+      </StepCtx.Provider>
+    </SpreadsheetCtx.Provider>
   );
 };
 

c70d45950b9ff4d13434325b4f367691825b6796
Marek <marek.firlejczyk@rst-it.com>
2019-03-01 13:29:08 +0100

    add variables as chips

--- src/components/Template/Editor.tsx
+++ src/components/Template/Editor.tsx
@@ -1,19 +1,34 @@
+import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import 'easymde/dist/easymde.min.css';
 import React from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 
 const Editor = () => {
-  const [input, setInput] = React.useState('Marek *jest* super');
+  const [content, setContent] = React.useState('');
+  const [variables] = React.useState<string[]>(['name', 'last_name', 'amount']);
   function setValue(value: string) {
-    setInput(value);
+    setContent(value);
   }
   return (
     <div style={{ padding: 20 }}>
       <Grid>
+        {variables.length ? (
+          <Paper style={{ padding: 20, margin: '20px 0' }}>
+            <Typography variant="button" gutterBottom={true}>
+              {'Available variables: '}
+            </Typography>
+            <hr />
+            {variables.map(variable => (
+              <Chip label={variable} style={{ margin: 5 }} />
+            ))}
+          </Paper>
+        ) : null}
         <SimpleMDE
           onChange={setValue}
-          value={input}
+          value={content}
           options={{
             autofocus: true,
             spellChecker: true,

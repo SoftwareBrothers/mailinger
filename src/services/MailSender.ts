0a9b09ba3f71232a583e4b53d19671a0bde83992
Paweł Lorenc <pawel.lorenc@rst-it.com>
2019-03-01 20:11:29 +0100

    Generate EML

--- src/services/MailSender.ts
+++ src/services/MailSender.ts
@@ -1,27 +1,18 @@
 import base64url from "base64url";
 import {IUser} from "../types";
 import {IRecipient} from "../types/recipient";
+import eml from "./GenerateEML"
 import googleApi from "./GoogleApi";
 
 const send = (recipients: IRecipient[], user: IUser) => {
 
   for (const recipient of recipients) {
 
-    const messageId = new Date().getUTCMilliseconds();
-
     const webSafe64 = (base64: any) => {
       return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
     };
 
-    const stream = `From: ${user.firstName} ${user.lastName} <${user.email}>
-To: ${recipient.firstName} ${recipient.lastName} <${recipient.email}>
-Reply-To: <${user.email}>
-Message-ID: ${messageId}
-Date: ${messageId}
-Subject: ${recipient.data.subject}
-    
-${recipient.data.content}`;
-
+    const stream = eml(recipient, user);
     const data = webSafe64(base64url(stream));
 
     googleApi.post('/gmail/v1/users/me/messages/send', {raw: data})

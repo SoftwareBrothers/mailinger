eeb61597fcc641388579077297fe06181a776c0d
Szymon Półtorak <poltorak.szymon@gmail.com>
2019-03-01 18:07:33 +0100

    solve conflict

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -5,13 +5,27 @@ import { SpreadsheetCtx } from 'src/contexts/spreadsheet.context';
 import { mailContent } from 'src/seeds/mail';
 
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+import { ISpreadsheet } from 'src/types/spreadsheet';
+import DrivePicker from '../DrivePicker';
+>>>>>>> Stashed changes
 import DynamicVariables from './DynamicVariables';
 
 const Editor = () => {
-  const [spreadsheet, setSpreadsheet] = React.useState(null);
+  const [spreadsheet, setSpreadsheet] = React.useState();
+  const [preview, setPreview] = React.useState('');
   let content = mailContent;
   function update(_: any, editor: any) {
     content = editor.getData();
+
+    // setPreview()
+    setPreview(content.replace(/\[(.*?)\]/g, (match, p1) => {
+      if (!!spreadsheet) {
+        return spreadsheet.usersData[0][p1]
+      }
+      return p1;
+    }))
   }
 
   return (
@@ -21,6 +35,8 @@ const Editor = () => {
           <DynamicVariables />
           <CKEditor editor={ClassicEditor} data={content} onChange={update} />
         </Grid>
+        <h3>Preview</h3>
+        <code>{ preview }</code>
       </SpreadsheetCtx.Provider>
     </div>
   );

22504210be00d1b8e2205203ff759c678086cfcf
Paweł Lorenc <pawel.lorenc@rst-it.com>
2019-03-02 00:07:17 +0100

    clean

--- src/components/SendEmailButton.tsx
+++ src/components/SendEmailButton.tsx
@@ -2,26 +2,11 @@ import Button from '@material-ui/core/Button';
 import React from 'react';
 import {UserCtx} from "../contexts/user.context";
 import send from '../services/MailSender';
-import {IRecipient} from "../types/recipient";
 
 const SendEmailButton = ({ rcps }) => {
-  console.log(rcps);
-
   const [user] = React.useContext(UserCtx);
 
   const click = () => {
-    const recipients: IRecipient[] = [
-      {
-        data: {
-          content: 'Masz te swoje 60k. Tutaj wyświetlam jakieś polskie znaki, na przykład w słowie żółć.',
-          subject: 'Wystaw fakturę',
-        },
-        email: 'pawel.lorenc@rst-it.com',
-        firstName: 'Paweł',
-        lastName: 'Lorenc',
-      }
-    ];
-
     send(rcps, user);
   };
 

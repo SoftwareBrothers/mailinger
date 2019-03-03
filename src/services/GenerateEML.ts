065efd489aefc8735748f77e7f79ff213f1ab5e2
Marek Firlejczyk <marekfirlejczyk@gmail.com>
2019-03-03 13:41:17 +0100

    Improve file after types improvments

--- src/services/GenerateEML.ts
+++ src/services/GenerateEML.ts
@@ -1,16 +1,14 @@
 import { Base64 } from 'js-base64';
-import {IUser} from "../types";
-import {IRecipient} from "../types/recipient";
+import { IRecipient, IUser } from '../types';
 
 export default (recipient: IRecipient, user: IUser) => {
-
   const messageId = new Date().getUTCMilliseconds();
   const userFullName = user.firstName + ' ' + user.lastName;
   const recipientFullName = recipient.firstName + ' ' + recipient.lastName;
 
   const encode = (text: any) => {
     return '=?utf-8?B?' + Base64.encodeURI(text) + '?=';
-  }
+  };
 
   return `From: ${encode(userFullName)} <${user.email}>
 To: ${encode(recipientFullName)} <${recipient.email}>
@@ -21,6 +19,4 @@ Subject: ${encode(recipient.data.subject)}
 Content-Type: text/html; charset="UTF-8"
 
 ${recipient.data.content}`;
-
 };
-

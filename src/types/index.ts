fa28dd72cf18a47a3544a479295f10cb0c2902fe
Marek Firlejczyk <marekfirlejczyk@gmail.com>
2019-03-03 13:40:55 +0100

    Write new interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,6 @@
-export * from './user';
+export * from './email';
 export * from './nullable';
+export * from './recipient';
+export * from './spreadsheet';
+export * from './step';
+export * from './user';

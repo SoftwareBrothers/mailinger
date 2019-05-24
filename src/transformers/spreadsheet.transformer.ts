a08eb04c00e14dd8b4033a3b8a18e219b575d167
michalzietek <michal.zietek@rst-it.com>
2019-05-24 20:26:00 +0200

    Collecting Spreadsheet as object with Sheets array

--- src/transformers/spreadsheet.transformer.ts
+++ src/transformers/spreadsheet.transformer.ts
@@ -1,8 +1,23 @@
-import { Spreadsheet } from 'models';
+import { Sheet, Spreadsheet } from 'models';
 
 export class SpreadSheetTransformer {
+
   public transform(data: any): Spreadsheet {
-    const arrayLike = data.sheets[0].data[0].rowData.map((row: any) => {
+    const transformedSheets = new Array();
+    const sheets = data.sheets;
+    for (const sheet of sheets) {
+      const transformedSheet = this.transformSheet(sheet);
+      transformedSheets.push(transformedSheet);
+    }
+
+    return {
+      sheets: transformedSheets
+    };
+  }
+
+
+  private transformSheet(sheet: any): Sheet {
+    const arrayLike = sheet.data[0].rowData.map((row: any) => {
       return row.values ? row.values.map((v: any) => v.formattedValue) : [];
     });
 
@@ -10,9 +25,9 @@ export class SpreadSheetTransformer {
       (row: any[]) => !row.every((v: any) => !v),
     );
 
-    const title = valuesWithoutEmpties[0].join('');
-    const variables = valuesWithoutEmpties[4];
-    const rawUsers = valuesWithoutEmpties.slice(5, valuesWithoutEmpties.length);
+    const title = this.getTitle(valuesWithoutEmpties);
+    const variables = this.getVariables(valuesWithoutEmpties);
+    const rawUsers = this.getRawUsers(valuesWithoutEmpties);
 
     const usersWithVars = rawUsers.map((userValues: any) => {
       const transformedUserObject = { title };
@@ -28,8 +43,21 @@ export class SpreadSheetTransformer {
     });
 
     return {
+      title: sheet.properties.title,
       usersData: usersWithVars,
-      variables: ['title'].concat(variables),
+      variables: ['title'].concat(variables)
     };
+  }
+
+  private getTitle(valuesWithoutEmpties: any) {
+    return valuesWithoutEmpties[0].join('');
+  }
+
+  private getVariables(valuesWithoutEmpties: any) {
+    return valuesWithoutEmpties[4];
+  }
+
+  private getRawUsers(valuesWithoutEmpties: any) {
+    return valuesWithoutEmpties.slice(5, valuesWithoutEmpties.length);
   }
 }
